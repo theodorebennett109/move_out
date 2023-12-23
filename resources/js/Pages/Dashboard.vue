525a6c5202fb54bc4cea9af546a12f8155d16a49
theodorebennett109 <theodorebennett3@gmail.com>
2023-12-22 22:05:58 -0500

    dummy data added to table

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -2,14 +2,40 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import GuestLayout from "@/Layouts/GuestLayout.vue";
 import { Head } from "@inertiajs/vue3";
+const props = defineProps(["carRentals"]);
 </script>
 
 <template>
     <Head title="Dashboard" />
 
     <GuestLayout>
-        <h2 class="text-xl font-semibold leading-tight text-gray-800">
+        <h2 class="my-3 text-xl font-semibold leading-tight text-gray-800">
             Dashboard
         </h2>
+        <!-- Table -->
+        <div>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Customer</th>
+                        <th scope="col">Car Model</th>
+                        <th scope="col">Rate/Day</th>
+                        <th scope="col">Total</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Invoice No.</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(carRental, index) in carRentals" :key="index">
+                        <td>{{ carRental.person.name }}</td>
+                        <td>{{ carRental.carModel }}</td>
+                        <td>{{ carRental.ratePerDay }}</td>
+                        <td>{{ carRental.total }}</td>
+                        <td>{{ carRental.status }}</td>
+                        <td>{{ carRental.invoiceNo }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </GuestLayout>
 </template>
--- routes/web.php
+++ routes/web.php
@@ -16,17 +16,108 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+
+
+$carRentals =[
+        ['person' => [
+            'name'=> "Theodore Bennett",
+            'address'=> "12 Norbrook",
+            'email'=> "theodorebennett3@gmail.com",
+            'number'=> 2818357],
+        'carModel'=> "Benz",
+        'ratePerDay'=> 2000,
+        'total'=> 10000,
+        'status'=> "Pending",
+        'invoiceNo'=> 12211 ],
+
+        ['person'=>[
+            'name'=> "Alice Johnson",
+            'address'=> "24 Oak Street",
+            'email'=> "alice.johnson@example.com",
+            'number'=> 9876543,
+        ],
+        'carModel'=> "Toyota",
+        'ratePerDay'=> 1500,
+        'total'=> 7500,
+        'status'=> "Approved",
+        'invoiceNo'=> 12212,
+    ],
+    [
+        'person'=>[
+            'name'=> "John Smith",
+            'address'=> "8 Elm Avenue",
+            'email'=> "john.smith@gmail.com",
+            'number'=> 5551234,
+        ],
+        'carModel'=> "Ford",
+        'ratePerDay'=> 1800,
+        'total'=> 9000,
+        'status'=> "Completed",
+        'invoiceNo'=> 12213,
+    ],
+    [
+        'person'=>[
+            'name'=> "Emma Davis",
+            'address'=> "15 Maple Lane",
+            'email'=> "emma.davis@example.com",
+            'number'=> 4567890,
+        ],
+        'carModel'=> "Honda",
+        'ratePerDay'=> 1700,
+        'total'=> 8500,
+        'status'=> "Pending",
+        'invoiceNo'=> 12214,
+    ],
+    [
+        'person'=>[
+            'name'=> "Oliver White",
+            'address'=> "5 Birch Street",
+            'email'=> "oliver.white@gmail.com",
+            'number'=> 1112233,
+        ],
+        'carModel'=> "Chevrolet",
+        'ratePerDay'=> 1900,
+        'total'=> 9500,
+        'status'=> "Approved",
+        'invoiceNo'=> 12215,
+    ],
+    [
+        'person'=>[
+            'name'=> "Sophia Martin",
+            'address'=> "18 Pine Road",
+            'email'=> "sophia.martin@example.com",
+            'number'=> 7778888,
+        ],
+        'carModel'=> "Nissan",
+        'ratePerDay'=> 1600,
+        'total'=> 8000,
+        'status'=> "Completed",
+        'invoiceNo'=> 12216,
+    ],
+    [
+        'person'=>[
+            'name'=> "Daniel Wilson",
+            'address'=> "3 Cedar Avenue",
+            'email'=> "daniel.wilson@gmail.com",
+            'number'=> 3334444,
+        ],
+        'carModel'=> "Tesla",
+        'ratePerDay'=> 2200,
+        'total'=> 11000,
+        'status'=> "Pending",
+        'invoiceNo'=> 12217, ]];
+
+    Route::get('/dashboard', function () use ($carRentals) {
+    return Inertia::render('Dashboard',['carRentals' => $carRentals]);
 })
 // ->middleware(['auth', 'verified'])
 ->name('dashboard');
